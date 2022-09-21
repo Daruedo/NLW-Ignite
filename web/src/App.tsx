9941b2c5759fd68c0851863c11eba05f2d70eb1c
Lucas Eduardo <daruedo@gmail.com>
2022-09-21 18:17:34 -0300

    Finalizando projeto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -72,6 +72,7 @@ function App() {
                                     <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
                                     <Input id="yearsPlaying" type="number" placeholder="Tudo bem ser ZERO"/>
                                 </div>
+
                                 <div className="flex flex-col gap-2">
                                     <label htmlFor="discord">Qual seu Discord?</label>
                                     <Input id="discord" type="text" placeholder="Usuario#0000"/>
@@ -81,6 +82,7 @@ function App() {
                             <div className="flex gap-6">
                                 <div className="flex flex-col gap-2">
                                     <label htmlFor="weekDays">Quando costuma jogar?</label>
+
                                     <div className="grid grid-cols-4 gap-2">
                                         <button type="button" className="w-8 h-8 rounded bg-zinc-900" title="Domingo">D</button>
                                         <button type="button" className="w-8 h-8 rounded bg-zinc-900" title="Segunda">S</button>
@@ -91,20 +93,25 @@ function App() {
                                         <button type="button" className="w-8 h-8 rounded bg-zinc-900" title="Sábado">S</button>
                                     </div>
                                 </div>
+
                                 <div className="flex flex-col gap-2 flex-1">
                                     <label htmlFor="hourStart">Qual o seu horário do dia?</label>
+
                                     <div className="grid grid-cols-2 gap-2">
                                         <Input id="hourStart" type="time" placeholder="De"/>
                                         <Input id="hourEnd" type="time" placeholder="Até"/>
                                     </div>
                                 </div>
                             </div>
+
                             <div className="mt-2 flex gap-2 text-sm">
                                 <Input type="checkbox"/>
                                 Costumo me conectar ao chat de voz
                             </div>
+
                             <footer className="mt-4 flex justify-end gap-4">
                                 <Dialog.Close className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">Cancelar</Dialog.Close>
+                                
                                 <button className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600" type="submit">
                                     <GameController className="w-6 h-6"/>
                                     Encontrar duo
